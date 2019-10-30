d86f600a86395c304ed3db971ba98d2091808d04
Carlos Loureda <balanzeneto@gmail.com>
2019-10-30 20:58:46 +0100

    unit-testing: Added custom mock for localStorage.
    Created an util called `MockedLocalStorage.js` to make available storage in the global object

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import "./utils/Tests/MockedLocalStorage";
+import App from "./App";
+
+/* Mocking LocalStorage to make App.test.js work */
+beforeAll(() => {
+  global.localStorage.setItem(
+    "token",
+    Math.random()
+      .toString(36)
+      .substr(-8)
+  );
+  console.log("global.localStorage: ", global.localStorage);
+});
+
+afterAll(() => {
+  global.localStorage.clear();
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+
   ReactDOM.render(<App />, div);
 });
---
+++ src/utils/Tests/MockedLocalStorage.js
@@ -0,0 +1,24 @@
+class MockedLocalStorage {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+// export default MockedLocalStorage;
+global.localStorage = new MockedLocalStorage();
